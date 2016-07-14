f6e242381a85c3f2818059ff55d67d18efac00b1
Brandon <mn.cres@gmail.com>
2016-07-14 12:28:11 -0500

    add new and create actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,25 @@
 class MessagesController < ApplicationController
-
   def index
   end
 
-  
+  def new
+    @message = Message.new
+  end
+
+  def create
+    # create a new instance of a message, require a message object from params and permit
+    # a title and a description. If it saves, redirect to the root otherwise render the new page
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:title, :description)
+    end
 
 end
