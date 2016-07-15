99dc23a136e4cd48b1ab214dc633c8c98f0ff4fd
Brandon <mn.cres@gmail.com>
2016-07-15 14:35:29 -0500

    comments model, controller, and views

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # TCTech is a forum for all things tech in the Twin Cities
+### Browse posts and sign up to contribute.
 
 * Ruby version
   2.2.3
 
 * Rails version
   5.0.0
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :messages
+
+  # Nest comments routes
+  resources :messages do
+    resources :comments
+  end
+  
   root 'messages#index'
 
 end
